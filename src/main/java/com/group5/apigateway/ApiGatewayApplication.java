eedfe44f1e779e9f027b440c4a2eef6af1ba24d0
Berkay Özerbay <ge29xof@mytum.de>
2022-02-02 15:12:14 +0100

    feat: draft of auth filter is added

--- src/main/java/com/group5/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/group5/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,35 @@ package com.group5.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+    //route should be added here
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("get_route", r -> r.path("/get")
+                        .method("GET")
+                        .uri("http://httpbin.org"))
+                .route("post_route", r -> r.path("/post")
+                        .method("POST")
+                        .uri("http://httpbin.org"))
+                .build();
     }
 
 }
