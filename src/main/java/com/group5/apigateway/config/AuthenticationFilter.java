4a14728467944602076a86eac8e8b6d03f6a8643
Yigit Kemal Erinc <erincyigit@gmail.com>
2022-02-06 02:22:55 +0100

    Unauthorize requests to restricted endpoints if no token is present

--- src/main/java/com/group5/apigateway/config/AuthenticationFilter.java
+++ src/main/java/com/group5/apigateway/config/AuthenticationFilter.java
@@ -1,10 +1,14 @@
 package com.group5.apigateway.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
@@ -15,13 +19,13 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import com.group5.apigateway.config.Request;
-
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 @RefreshScope
 @Component
+@Slf4j
 public class AuthenticationFilter implements GatewayFilter {
 
     /* The endpoints in here can be accessed without any permission
@@ -36,22 +40,22 @@ public class AuthenticationFilter implements GatewayFilter {
      * It is not accessible from outside and only allowed to be called by other services
      */
     private final Map<Request, ImmutableList<String>> PERMISSIONED_ENDPOINTS = new HashMap<>(){{
-        put(new Request(HttpMethod.GET, Pattern.compile("\\/user\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
-        //put(new Request(HttpMethod.POST, "/users/*"), ImmutableList.of("DISPATCHER"));
-        //put(new Request(HttpMethod.GET, "/users/*/role"), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.GET, Pattern.compile("\\/api\\/cas\\/users\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.POST, Pattern.compile("\\/api\\/cas\\/users\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.GET, Pattern.compile("\\/api\\/cas\\/users\\/[a-zA-Z0-9]*\\/role")), ImmutableList.of("DISPATCHER"));
 
-        //put(new Request(HttpMethod.POST, "/boxes"), ImmutableList.of("DISPATCHER"));
-        //put(new Request(HttpMethod.PUT, "/boxes/*"), ImmutableList.of("DISPATCHER"));
-        //put(new Request(HttpMethod.DELETE, "/boxes/*"), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.POST, Pattern.compile("\\/api\\/ds\\/boxes")), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.PUT, Pattern.compile("\\/api\\/ds\\/boxes\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.DELETE, Pattern.compile("\\/api\\/ds\\/boxes\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
 
-        put(new Request(HttpMethod.PUT, Pattern.compile("\\/collected\\/([0-9]+)(\\-)([0-9]+)\\/deliverer\\/([0-9]+)(\\-)([0-9]+)")), ImmutableList.of("DISPATCHER"));
-        //put(new Request(HttpMethod.PUT, "/*/deposited/deliverer/*/box/*"), ImmutableList.of("DISPATCHER"));
-        // put(new Request(HttpMethod.PUT, "/user/*/delivered/box/*"), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.PUT, Pattern.compile("\\/api\\/ds\\/collected\\/[a-zA-Z0-9]*\\/deliverer\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.PUT, Pattern.compile("\\/api\\/ds\\/deliverer\\/[a-zA-Z0-9]*\\/deposited\\/box\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.PUT, Pattern.compile("\\/api\\/ds\\/user\\/[a-zA-Z0-9]*\\/delivered\\/box\\/[a-zA-Z0-9]*")), ImmutableList.of("DISPATCHER"));
 
-        //put(new Request(HttpMethod.GET, "/customer/*/status/delivered"), ImmutableList.of("DISPATCHER", "CUSTOMER")); // Only the customer with that id
-        //put(new Request(HttpMethod.GET, "/customer/{customerId}/status/active"), ImmutableList.of("DISPATCHER", "CUSTOMER")); // Only the customer with that id
+        put(new Request(HttpMethod.GET, Pattern.compile("\\/api\\/ds\\/customer\\/[a-zA-Z0-9]*\\/status\\/delivered")), ImmutableList.of("DISPATCHER", "CUSTOMER")); // Only the customer with that id
+        put(new Request(HttpMethod.GET, Pattern.compile("\\/api\\/ds\\/customer\\/[a-zA-Z0-9]*\\/status\\/active")), ImmutableList.of("DISPATCHER", "CUSTOMER")); // Only the customer with that id
 
-        //put(new Request(HttpMethod.POST, "/deliveries"), ImmutableList.of("DISPATCHER"));
+        put(new Request(HttpMethod.POST, Pattern.compile("\\/api\\/ds\\/deliveries")), ImmutableList.of("DISPATCHER"));
     }};
 
     private final RestTemplate restTemplate;
@@ -65,19 +69,47 @@ public class AuthenticationFilter implements GatewayFilter {
         ServerHttpRequest request = exchange.getRequest();
         var path = request.getURI().getPath();
         var allowedEndpoint = PERMISSIONLESS_ENDPOINTS.keySet()
-                .stream().anyMatch(url -> path.equals(url) && request.getMethod().equals(PERMISSIONED_ENDPOINTS.get(url)));
+                .stream().anyMatch(matchesUrlAndHttpMethod(request, path));
 
         if (allowedEndpoint) {
             return chain.filter(exchange);
         }
 
         for (Request reqDefinition : PERMISSIONED_ENDPOINTS.keySet()) {
-            if (reqDefinition.pattern.matcher(path).matches()) {
-                System.out.println("MATCHED");
+            if (reqDefinition.getPattern().matcher(path).matches()) {
+                if (!request.getHeaders().containsKey("Authorization")) {
+                    var response = exchange.getResponse();
+                    Map<String, Object> responseData = Maps.newHashMap();
+                    responseData.put("code", 401);
+                    responseData.put("message", "Illegal request");
+                    responseData.put("cause", "Token is empty");
+
+                    try {
+                        ObjectMapper objectMapper = new ObjectMapper();
+                        byte[] data = objectMapper.writeValueAsBytes(responseData);
+
+                        DataBuffer buffer = response.bufferFactory().wrap(data);
+                        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                        response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+                        return response.writeWith(Mono.just(buffer));
+                    } catch (JsonProcessingException e) {
+                        log.error("{}", e);
+                    }
+                }
+
             }
         }
 
         return chain.filter(exchange);
+    }
+
+    private String getUserRole(final String userId) {
+        var url = String.format("lb://customer-authentication-service/%s/role", userId);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    private Predicate<String> matchesUrlAndHttpMethod(ServerHttpRequest request, String path) {
+        return url -> path.equals(url) && request.getMethod().equals(PERMISSIONED_ENDPOINTS.get(url));
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
